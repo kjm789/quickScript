24ef9f9cdb39589aed3bba8da375f9d8bf56394f
Michael <mjk8v2@mail.missouri.edu>
2016-06-02 13:27:36 -0600

    removed

--- secure/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-	DEFINE( "HOST", "host=$OPENSHIFT_POSTGRESQL_DB_HOST" );
-	DEFINE( "DBNAME", "dbname=quickscript" );
-	DEFINE( "USERNAME", "user=adminlw7jdp4" );
-	DEFINE( "PASSWORD", "password=GEf_VrblWlVn" );
-?>
